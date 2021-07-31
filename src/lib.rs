adaf66a16178c04ff9f2daf144e022464c28d80f
Michael J Ward <ward.michael.j@gmail.com>
2021-07-31 12:50:40 -0500

    applied callback_if to enum make_proj_ty

--- src/lib.rs
+++ src/lib.rs
@@ -597,39 +597,45 @@ macro_rules! __pin_project_internal {
             ),+
         }
 
-        $crate::__pin_project_internal! { @enum=>make_proj_ty;
-            [$proj_vis]
-            [$($proj_mut_ident)?]
-            [make_proj_field_mut]
-            [$ident]
-            [$($impl_generics)*] [$($ty_generics)*] [$(where $($where_clause)*)?]
-            {
-                $(
-                    $variant $({
-                        $(
-                            $(#[$pin])?
-                            $field: $field_ty
-                        ),+
-                    })?
-                ),+
-            }
+        $crate::__pin_project_internal! { @callback_if;
+            [conditional $($proj_mut_ident)?]
+            [cb enum make_proj_ty]
+            [args
+                [$proj_vis]
+                [make_proj_field_mut]
+                [$ident]
+                [$($impl_generics)*] [$($ty_generics)*] [$(where $($where_clause)*)?]
+                {
+                    $(
+                        $variant $({
+                            $(
+                                $(#[$pin])?
+                                $field: $field_ty
+                            ),+
+                        })?
+                    ),+
+                }
+            ]
         }
-        $crate::__pin_project_internal! { @enum=>make_proj_ty;
-            [$proj_vis]
-            [$($proj_ref_ident)?]
-            [make_proj_field_ref]
-            [$ident]
-            [$($impl_generics)*] [$($ty_generics)*] [$(where $($where_clause)*)?]
-            {
-                $(
-                    $variant $({
-                        $(
-                            $(#[$pin])?
-                            $field: $field_ty
-                        ),+
-                    })?
-                ),+
-            }
+        $crate::__pin_project_internal! { @callback_if;
+            [conditional $($proj_ref_ident)?]
+            [cb enum make_proj_ty]
+            [args
+                [$proj_vis]
+                [make_proj_field_ref]
+                [$ident]
+                [$($impl_generics)*] [$($ty_generics)*] [$(where $($where_clause)*)?]
+                {
+                    $(
+                        $variant $({
+                            $(
+                                $(#[$pin])?
+                                $field: $field_ty
+                            ),+
+                        })?
+                    ),+
+                }
+            ]
         }
         $crate::__pin_project_internal! { @enum=>make_proj_replace_ty;
             [$proj_vis]
@@ -873,8 +879,8 @@ macro_rules! __pin_project_internal {
     // =============================================================================================
     // enum:make_proj_ty
     (@enum=>make_proj_ty;
-        [$proj_vis:vis]
         [$proj_ty_ident:ident]
+        [$proj_vis:vis]
         [$make_proj_field:ident]
         [$ident:ident]
         [$($impl_generics:tt)*] [$($ty_generics:tt)*] [$(where $($where_clause:tt)* )?]
@@ -914,15 +920,6 @@ macro_rules! __pin_project_internal {
             ),+
         }
     };
-    (@enum=>make_proj_ty;
-        [$proj_vis:vis]
-        []
-        [$make_proj_field:ident]
-        [$ident:ident]
-        [$($impl_generics:tt)*] [$($ty_generics:tt)*] [$(where $($where_clause:tt)* )?]
-        $($variant:tt)*
-    ) => {};
-
     (@enum=>make_proj_replace_ty;
         [$proj_vis:vis]
         [$proj_ty_ident:ident]
